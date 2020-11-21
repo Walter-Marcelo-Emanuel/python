376089da474c0b03d5874c2a922c99287514d271
Walter-Macelo-Emanuel <wappolonia@gmail.com@users.noreply.github.com>
2020-11-21 19:18:38 -0300

    dado

---
+++ dados.py
@@ -0,0 +1,13 @@
+#_*_ coding: UTF-8_*_
+
+seguir= True
+while seguir:
+    import random
+    dado1= random.randint(1,6)
+    dado2=random.randint(1,6)
+    puntos=(dado1+dado2)
+    print("dado 1:",dado1)
+    print("dado 2:",dado2)
+    print("dsuman ambos", puntos)
+    seuir= ("s" ==input("¿volveer a tirar? Si o No")).lower()
+    
